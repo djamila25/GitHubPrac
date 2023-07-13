5065d425ef855527fc17acc95214d255cf8f015a
Djamila <djamilanjikam6@gmail.com>
2023-07-13 13:30:08 -0500

    Initial Commit

---
+++ GitHubPrac/GitHubPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPracApp.swift
+//  GitHubPrac
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
